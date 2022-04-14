f0ba2773ebac6050bc96deaf168f6e91e5fe8377
Rodrigo Costa <rodrigo.costa@jbq.global>
2022-04-14 06:26:32 -0300

    [master] subida inicial do projeto

---
+++ agf-sboot-bff/src/main/java/br/com/rcm/agf/agfsbootbff/AgfSbootBffApplication.java
@@ -0,0 +1,13 @@
+package br.com.rcm.agf.agfsbootbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgfSbootBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgfSbootBffApplication.class, args);
+	}
+
+}
---
+++ agf-sboot-bff/src/test/java/br/com/rcm/agf/agfsbootbff/AgfSbootBffApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rcm.agf.agfsbootbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgfSbootBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
